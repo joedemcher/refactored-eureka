3ea76453d73cb84fee02c2b1250cf4d5fe5ab0ac
Joseph Demcher <jpd29@njit.edu>
2022-06-29 14:43:05 -0400

    Add files via upload

--- movie-theater-src/src/test/java/com/jpmc/theater/MovieTests.java
+++ movie-theater-src/src/test/java/com/jpmc/theater/MovieTests.java
@@ -16,4 +16,21 @@ public class MovieTests {
     Showing showing = new Showing(spiderMan, 5, LocalDateTime.of(LocalDate.now(), LocalTime.now()));
     assertEquals(10, spiderMan.calculateTicketPrice(showing));
   }
+
+  @Test
+  void afternoonMovieDiscount() {
+    Movie spiderMan = new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90), 10, 1);
+    LocalTime noon = LocalTime.of(12, 00, 0);
+    Showing showing = new Showing(spiderMan, 5, LocalDateTime.of(LocalDate.now(), noon));
+    assertEquals(7.5, spiderMan.calculateTicketPrice(showing));
+  }
+
+  @Test
+  void seventhDayOfMonthDiscount() {
+    Movie spiderMan = new Movie("Spider-Man: No Way Home", Duration.ofMinutes(90), 10, 3);
+    LocalDate seventh = LocalDate.of(2022, 01, 7);
+    LocalTime fiveThirtyPm = LocalTime.of(17,30,00);
+    Showing showing = new Showing(spiderMan, 5, LocalDateTime.of(seventh, fiveThirtyPm));
+    assertEquals(9, spiderMan.calculateTicketPrice(showing));
+  }
 }
